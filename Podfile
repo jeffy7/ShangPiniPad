185bfa24ab4117c0ec1f7fdaec41b8e9b88f782d
je_ffy <jzxiaobao@gmail.com>
2016-12-04 11:49:24 +0800

    add cocoa pods Aalmofire

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+platform :ios, '8.0'
+use_frameworks!
+
+target 'ShangPiniPad' do 
+	pod 'Alamofire' ,'~> 3.0'
+
+end
---
+++ Pods/Target Support Files/Pods-ShangPiniPad/Pods-ShangPiniPad-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ShangPiniPadVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ShangPiniPadVersionString[];
+
